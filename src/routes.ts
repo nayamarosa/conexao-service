1be8d4fb356adb5a978d53e2f2ea1136dab051ed
NayamaRosa <falecom@nayama.com.br>
2022-09-16 21:50:19 -0300

    changing routes name

--- src/routes.ts
+++ src/routes.ts
@@ -2,17 +2,20 @@
 import { Router } from 'express';
 import { createMentorController } from './Mentor/useCases/createMentor';
 import { createMentoredController } from './Mentored/useCases/createMentored';
+import { getAllMentorController } from './Mentor/useCases/getAllMentor';
 
 const router = Router();
 
-router.post('/create-mentor', (request, response) => {
+router.post('/mentor', (request, response) => {
   return createMentorController.handle(request, response);
 });
 
-router.post('/create-mentored', (request, response) => {
-  console.log('mentored');
+router.post('/mentored', (request, response) => {
   return createMentoredController.handle(request, response);
+});
 
+router.get('/mentor', (request, response) => {
+  return getAllMentorController.handle(request, response);
 });
 
 export { router }
