7fccc07dfe34b516ffe2d23573059b47abdeb3e2
Nayama Rosa <npereira@bionexo.com>
2022-09-30 22:17:32 -0300

    editing postgres path

--- src/db/conection.js
+++ src/db/conection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('postgres://cmymectxfzchjp:8b90c867378d8bd471b4f00f4f633002af67e6e12fd783008e3b29bc2c2a2789@ec2-44-195-132-31.compute-1.amazonaws.com:5432/dadcvudd04t0q5', {
+const sequelize = new Sequelize('postgres://vihajicmjplljy:103645aae3759b81db7bba8a840a470f1ee0b4f9efbb01d64c1874f148657678@ec2-34-199-68-114.compute-1.amazonaws.com:5432/da9g3350ujfgik', {
   dialectOptions: {
     ssl: {
       rejectUnauthorized: false,
